09f9a4021534ae2f9d23baacb115885f04547aef
Sam Garg <34547038+imsamgarg@users.noreply.github.com>
2024-01-13 00:17:50 +0530

    Fixes prefer_async_callback showing for Future type that has no type param or other than void (#19)

--- lib/src/lints/flutter/prefer_async_callback.dart
+++ lib/src/lints/flutter/prefer_async_callback.dart
@@ -1,3 +1,5 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/error/error.dart';
 import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
@@ -28,8 +30,8 @@ class PreferAsyncCallback extends DartLintRule {
     if (!context.pubspec.isFlutterProject) return;
 
     context.registry.addGenericFunctionType((node) {
-      final returnType = node.returnType?.type;
-      if (returnType?.isDartAsyncFuture != true) return;
+      final returnType = node.returnType;
+      if (returnType == null || !_isFutureVoid(returnType)) return;
 
       final parameters = node.parameters.parameters;
       if (parameters.isNotEmpty) return;
@@ -39,6 +41,20 @@ class PreferAsyncCallback extends DartLintRule {
 
       reporter.reportErrorForNode(code, node);
     });
+  }
+
+  bool _isFutureVoid(TypeAnnotation typeAnnotation) {
+    final type = typeAnnotation.type;
+    if (type == null || !type.isDartAsyncFuture) return false;
+
+    // Since we know it's a Future, we can safely cast it to NamedType.
+    typeAnnotation as NamedType;
+    final typeArgs = typeAnnotation.typeArguments?.arguments;
+
+    return switch (typeArgs) {
+      [final NamedType type] when type.type is VoidType => true,
+      _ => false,
+    };
   }
 
   @override
