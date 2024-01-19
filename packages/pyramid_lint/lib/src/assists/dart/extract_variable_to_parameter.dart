fc4b715132d87962ba7550430220bdd315f2132a
Sam Garg <smlgarg123@gmail.com>
2024-01-19 22:15:01 +0530

    separate assist for named and positional parameters

--- packages/pyramid_lint/lib/src/assists/dart/extract_variable_to_parameter.dart
+++ packages/pyramid_lint/lib/src/assists/dart/extract_variable_to_parameter.dart
@@ -1,8 +1,8 @@
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/source/source_range.dart';
+import 'package:analyzer_plugin/utilities/change_builder/change_builder_dart.dart';
 import 'package:collection/collection.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
@@ -17,107 +17,132 @@ class ExtractVariableToParameter extends DartAssist {
     context.registry.addVariableDeclaration((node) {
       if (!node.sourceRange.covers(target)) return;
 
-      final elem = node.declaredElement;
-      if (elem == null || elem is! LocalVariableElement) return;
-
-      final functionDeclaration = node.thisOrAncestorMatching(
-        (e) => e is MethodDeclaration || e is FunctionDeclaration,
-      );
-
-      final variableName = elem.name;
-      final variableType = elem.type;
-      final variableTypeName =
-          variableType.getDisplayString(withNullability: true);
-
-      if (functionDeclaration == null) return;
-
-      int offset;
-      final isNamedParam = !variableName.startsWith('_');
-
-      final isRequired = isNamedParam &&
-          !(variableTypeName.endsWith('?') || variableType is DynamicType);
-
-      var addNameParamBrackets = isNamedParam;
-
-      final FormalParameterList? paramList;
-
-      if (functionDeclaration is MethodDeclaration) {
-        paramList = functionDeclaration.parameters;
-      } else {
-        functionDeclaration as FunctionDeclaration;
-        paramList = functionDeclaration.functionExpression.parameters;
-      }
-
+      final paramList = _getParameterList(node);
       if (paramList == null) return;
 
-      final (calculatedOffset, hasANamedParam) =
-          getOffset(paramList, variableName);
-      if (calculatedOffset == null) return;
-      offset = calculatedOffset;
-      addNameParamBrackets = !hasANamedParam;
+      final variableName = node.name.lexeme;
+      final paramAlreadyExists = paramList.parameters
+              .firstWhereOrNull((e) => e.name?.lexeme == variableName) !=
+          null;
+      if (paramAlreadyExists) return;
 
       final varDeclarationStatement =
-          node.thisOrAncestorOfType<VariableDeclarationStatement>();
+          node.thisOrAncestorOfType<VariableDeclarationStatement>()!;
 
-      final changeBuilder = reporter.createChangeBuilder(
-        message: 'Move variable to parameter list',
+      final positionalParamChangeBuilder = reporter.createChangeBuilder(
+        message: 'Extract variable as parameter',
+        priority: 79,
+      );
+
+      final namedParamChangeBuilder = reporter.createChangeBuilder(
+        message: 'Extract variable as named parameter',
         priority: 80,
       );
 
-      changeBuilder.addDartFileEdit((builder) {
+      final variableTypeName =
+          node.declaredElement!.type.getDisplayString(withNullability: true);
+      final namedParams = paramList.parameters.where((e) => e.isNamed);
+      final positionalParams =
+          paramList.parameters.where((e) => e.isPositional);
+
+      positionalParamChangeBuilder.addDartFileEdit((builder) {
+        int offset;
+        var addPreceedingComma = false;
+        var addSucceedingComma = false;
+
+        if (positionalParams.isEmpty) {
+          offset = paramList.leftParenthesis.end;
+          addSucceedingComma = namedParams.isNotEmpty;
+        } else {
+          addPreceedingComma = true;
+          offset = positionalParams.last.endToken.next!.offset;
+        }
+
         builder.addInsertion(offset, (builder) {
-          if (!isNamedParam) {
-            builder.write('$variableTypeName $variableName');
-            return;
-          }
-
-          if (addNameParamBrackets) builder.write('{');
-          if (isRequired) builder.write('required ');
-
+          if (addPreceedingComma) builder.write(',');
           builder.write('$variableTypeName $variableName');
-
-          if (addNameParamBrackets) builder.write('}');
+          if (addSucceedingComma) builder.write(',');
         });
 
-        final variables = varDeclarationStatement!.variables.variables;
+        _deleteOriginalVariable(varDeclarationStatement, builder, node);
+      });
 
-        // Delete the entire expression if there's only one variable
-        if (variables.length == 1) {
-          builder.addDeletion(varDeclarationStatement.sourceRange);
+      namedParamChangeBuilder.addDartFileEdit((builder) {
+        int offset;
+        var addBracketPreceedingComma = false;
+        var addEnclosingBrackets = false;
+        var addPreceedingComma = false;
+
+        if (namedParams.isEmpty && positionalParams.isEmpty) {
+          addEnclosingBrackets = true;
+          offset = paramList.leftParenthesis.end;
+        } else if (namedParams.isEmpty && positionalParams.isNotEmpty) {
+          final param = positionalParams.last;
+          addEnclosingBrackets = true;
+          addBracketPreceedingComma =
+              param.endToken.next!.type != TokenType.COMMA;
+          offset = param.end;
         } else {
-          // We need to also delete the comma in case of multiple variables.
-          SourceRange range;
-
-          // If the next token is "comma" means the variable is in the middle or
-          // at the begining.
-          if (node.endToken.next?.type == TokenType.COMMA) {
-            range = node.sourceRange.getUnion(node.endToken.next!.sourceRange);
-          } else {
-            // Variable is at the last.
-            range = node.sourceRange
-                .getUnion(node.beginToken.previous!.sourceRange);
-          }
-
-          builder.addDeletion(range);
+          final param = namedParams.last;
+          addPreceedingComma = param.endToken.next!.type != TokenType.COMMA;
+          offset = param.end;
         }
+
+        builder.addInsertion(offset, (builder) {
+          if (addBracketPreceedingComma) builder.write(',');
+          if (addEnclosingBrackets) builder.write('{');
+          if (addPreceedingComma) builder.write(',');
+          if (!variableTypeName.endsWith('?')) builder.write('required ');
+          builder.write('$variableTypeName $variableName');
+          if (addEnclosingBrackets) builder.write('}');
+        });
+
+        _deleteOriginalVariable(varDeclarationStatement, builder, node);
       });
     });
   }
 
-  (int?, bool) getOffset(FormalParameterList paramList, String varName) {
-    final paramElems = paramList.parameterElements.whereNotNull();
+  FormalParameterList? _getParameterList(VariableDeclaration node) {
+    final elem = node.declaredElement;
+    if (elem == null || elem is! LocalVariableElement) return null;
 
-    if (paramElems.isEmpty) {
-      return (paramList.leftParenthesis.end, false);
+    final functionDeclaration = node.thisOrAncestorMatching(
+      (e) => e is MethodDeclaration || e is FunctionDeclaration,
+    );
+
+    return switch (functionDeclaration) {
+      MethodDeclaration() => functionDeclaration.parameters,
+      FunctionDeclaration() =>
+        functionDeclaration.functionExpression.parameters,
+      _ => null
+    };
+  }
+
+  void _deleteOriginalVariable(
+    VariableDeclarationStatement varDeclarationStatement,
+    DartFileEditBuilder builder,
+    VariableDeclaration node,
+  ) {
+    final variables = varDeclarationStatement.variables.variables;
+
+    // Delete the entire expression if there's only one variable
+    if (variables.length == 1) {
+      builder.addDeletion(varDeclarationStatement.sourceRange);
     } else {
-      final alreadyHasAParam =
-          paramElems.firstWhereOrNull((e) => e.name == varName) == null;
+      // We need to also delete the comma in case of multiple variables.
+      SourceRange range;
 
-      if (alreadyHasAParam) return (null, false);
-      final hasANamedParam =
-          paramElems.firstWhereOrNull((e) => e.isNamed) != null;
+      // If the next token is "comma" means the variable is in the middle or
+      // at the begining.
+      if (node.endToken.next?.type == TokenType.COMMA) {
+        range = node.sourceRange.getUnion(node.endToken.next!.sourceRange);
+      } else {
+        // Variable is at the last.
+        range =
+            node.sourceRange.getUnion(node.beginToken.previous!.sourceRange);
+      }
 
-      return (paramList.rightParenthesis.offset, hasANamedParam);
+      builder.addDeletion(range);
     }
   }
 }
