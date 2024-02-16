82d14d85bc7a711a0eb436094c47a890b37b80fb
Charles Tsang <charles.cy.tsang@gmail.com>
2024-02-17 05:30:24 +0800

    Add new lint avoid_mutable_global_variables (#37)

---
+++ packages/pyramid_lint_test/test/lints/dart/avoid_mutable_global_variables.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: avoid_multiple_declarations_per_line
+
+// expect_lint: avoid_mutable_global_variables
+double pi = 3.14;
+
+// expect_lint: avoid_mutable_global_variables
+String a = 'a', b = 'b';
