a9f7c05075f897f43b8e9cd93f2ea1a6db03c2ef
Charles Tsang <charles.cy.tsang@gmail.com>
2024-02-24 18:32:20 +0800

    Fix false positive for avoid_nested_if (#44)

--- packages/pyramid_lint/lib/src/lints/dart/avoid_nested_if.dart
+++ packages/pyramid_lint/lib/src/lints/dart/avoid_nested_if.dart
@@ -5,9 +5,9 @@ import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:meta/meta.dart' show immutable;
 
 import '../../pyramid_lint_rule.dart';
-import '../../utils/ast_node_extensions.dart';
 import '../../utils/constants.dart';
 import '../../utils/typedef.dart';
+import '../../utils/visitors.dart';
 
 @immutable
 class AvoidNestedIfOptions {
@@ -61,10 +61,12 @@ class AvoidNestedIf extends PyramidLintRule<AvoidNestedIfOptions> {
     CustomLintContext context,
   ) {
     context.registry.addIfStatement((node) {
-      final parentIf = node.parent?.thisOrAncestorOfType<IfStatement>();
-      if (parentIf != null) return;
+      final ifStatements = <IfStatement>[];
+      final visitor = RecursiveIfStatementVisitor(
+        onVisitIfStatement: ifStatements.add,
+      );
+      node.thenStatement.visitChildren(visitor);
 
-      final ifStatements = node.childrenIfStatements;
       if (ifStatements.length < options.params.maxNestingLevel) return;
 
       reporter.reportErrorForNode(code, node);
--- packages/pyramid_lint_test/test/lints/dart/avoid_nested_if.dart
+++ packages/pyramid_lint_test/test/lints/dart/avoid_nested_if.dart
@@ -1,11 +1,21 @@
-// ignore_for_file: literal_only_boolean_expressions, avoid_empty_blocks, dead_code
+// ignore_for_file: literal_only_boolean_expressions, avoid_empty_blocks
 
-void example() {
+void fn() {
   // expect_lint: avoid_nested_if
   if (true) {
     if (true) {
-      if (true) {
-      } else {}
-    } else {}
-  } else {}
+      if (true) {}
+    }
+  }
+}
+
+void fn2(int a) {
+  if (a == 1) {
+  } else if (a == 2) {
+    // expect_lint: avoid_nested_if
+  } else if (a == 3) {
+    if (true) {
+      if (true) {}
+    }
+  } else if (a == 4) {}
 }
