3f55816ae1c15cb43c927e5571e3e9b257f1944f
Charles Tsang <charles.cy.tsang@gmail.com>
2025-04-17 13:06:30 +0800

    Fix prefer_const_constructor_declarations to support constructors with super parameters

--- packages/pyramid_lint_test/test/lints/dart/prefer_const_constructor_declarations/prefer_const_constructor_declarations.dart
+++ packages/pyramid_lint_test/test/lints/dart/prefer_const_constructor_declarations/prefer_const_constructor_declarations.dart
@@ -7,7 +7,7 @@ class Point {
   // expect_lint: prefer_const_constructor_declarations
   Point(this.x, this.y);
 
-  // The lint will not be triggered because the super constructor is not const.
+  // The lint will not be triggered because the redirecting constructor is not const.
   Point.origin() : this(0, 0);
 
   // expect_lint: prefer_const_constructor_declarations
@@ -25,10 +25,32 @@ class Point {
 class Point3D extends Point {
   final double z;
 
+  // The lint will not be triggered because the super constructor is not const.
   Point3D(super.x, super.y, this.z);
 
   // The lint will not be triggered because the super constructor is not const.
   Point3D.origin()
+      : z = 0,
+        super.origin();
+}
+
+class Coordinate {
+  final double x;
+  final double y;
+
+  const Coordinate(this.x, this.y);
+
+  const Coordinate.origin() : this(0, 0);
+}
+
+class Coordinate3D extends Coordinate {
+  final double z;
+
+  // expect_lint: prefer_const_constructor_declarations
+  Coordinate3D(super.x, super.y, this.z);
+
+  // expect_lint: prefer_const_constructor_declarations
+  Coordinate3D.origin()
       : z = 0,
         super.origin();
 }
