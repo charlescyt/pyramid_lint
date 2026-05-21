42e148deff5a7a6d7db1c06d3e5cc63ce07d40a6
Charles Tsang <charles.cy.tsang@gmail.com>
2026-05-21 22:55:49 +0800

    Migrate prefer_void_callback

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'src/rules/dart/prefer_iterable_every.dart';
 import 'src/rules/dart/prefer_iterable_first.dart';
 import 'src/rules/dart/prefer_iterable_last.dart';
 import 'src/rules/dart/prefer_new_line_before_return.dart';
+import 'src/rules/flutter/prefer_void_callback.dart';
 import 'src/rules/flutter/proper_expanded_and_flexible.dart';
 import 'src/rules/flutter/use_spacer.dart';
 
@@ -44,6 +45,8 @@ class PyramidLintPlugin extends Plugin {
       ..registerFixForRule(PreferNewLineBeforeReturnRule.code, AddNewLineBeforeReturn.new);
 
     registry
+      ..registerLintRule(PreferVoidCallbackRule())
+      ..registerFixForRule(PreferVoidCallbackRule.code, ReplaceWithVoidCallback.new)
       ..registerLintRule(ProperExpandedAndFlexibleRule())
       ..registerLintRule(UseSpacerRule())
       ..registerFixForRule(UseSpacerRule.code, ReplaceWithSpacer.new);
--- packages/pyramid_lint_test/test/lints/flutter/prefer_void_callback/prefer_void_callback.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart' show VoidCallback;
-
-typedef A = VoidCallback;
-// expect_lint: prefer_void_callback
-typedef B = void Function();
-// expect_lint: prefer_void_callback
-typedef C = void Function()?;
-typedef D = void Function(int index);
-typedef E = void Function({required int index});
