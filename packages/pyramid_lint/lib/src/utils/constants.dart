e4e3242297122f49084b10b9ebeee099169953b3
Charles Tsang <charles.cy.tsang@gmail.com>
2026-05-26 15:17:40 +0800

    Remove legacy files

--- packages/pyramid_lint/lib/src/utils/constants.dart
+++
@@ -1,3 +0,0 @@
-const docUrl = 'https://docs.page/charlescyt/pyramid_lint';
-const dartLintDocUrl = '$docUrl/dart-lints';
-const flutterLintDocUrl = '$docUrl/flutter-lints';
--- packages/pyramid_lint/lib/src/utils/pubspec_extension.dart
+++
@@ -1,8 +0,0 @@
-import 'package:pub_semver/pub_semver.dart';
-import 'package:pubspec_parse/pubspec_parse.dart';
-
-extension PubspecExtension on Pubspec {
-  bool get isFlutterProject => dependencies.containsKey('flutter');
-
-  VersionConstraint? get dartSdkVersion => environment['sdk'];
-}
