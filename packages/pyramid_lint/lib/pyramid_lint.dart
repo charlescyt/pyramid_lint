e97af2a27a35fcf1cbf4fabaf6d732cfb7d3565b
Charles Tsang <charles.cy.tsang@gmail.com>
2026-05-26 15:10:45 +0800

    Remove assists
    - invert_boolean_expression
    - swap_then_else_expression
    - use_edge_insets_zero
    - wrap_all_children_with_expanded
    - wrap_with_layout_builder
    - wrap_with_listenable_builder

--- packages/pyramid_lint/lib/pyramid_lint.dart
+++ packages/pyramid_lint/lib/pyramid_lint.dart
@@ -1,12 +1,5 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
-import 'src/assists/dart/invert_boolean_expression.dart';
-import 'src/assists/dart/swap_then_else_expression.dart';
-import 'src/assists/flutter/use_edge_insets_zero.dart';
-import 'src/assists/flutter/wrap_all_children_with_expanded.dart';
-import 'src/assists/flutter/wrap_with_layout_builder.dart';
-import 'src/assists/flutter/wrap_with_listenable_builder.dart';
-
 /// This is the entry point of Pyramid Linter.
 PluginBase createPlugin() => _PyramidLinter();
 
@@ -20,12 +13,6 @@ class _PyramidLinter extends PluginBase {
   @override
   List<Assist> getAssists() => [
     // Dart assists
-    InvertBooleanExpression(),
-    SwapThenElseExpression(),
     // Flutter assists
-    UseEdgeInsetsZero(),
-    WrapAllChildrenWithExpanded(),
-    WrapWithLayoutBuilder(),
-    WrapWithListenableBuilder(),
   ];
 }
